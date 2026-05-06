cb320e15a8022ca6fe1e7c138a2a78597de1d1a4
unknown <vodanhgd@gmail.com>
2026-05-07 02:19:18 +0700

    su dung bien moi truong cho DB

--- config/database.php
+++ config/database.php
@@ -1,8 +1,7 @@
 <?php
 
 // config/database.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-// File: config/database.php
-define('DB_NAME', 'pharmacy_db'); 
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'pharmacy_db');
