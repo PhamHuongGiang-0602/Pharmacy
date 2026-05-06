7a88a3e292ea775ddff5a7ebeff20c61c72c18e7
PhamHuongGiang-0602 <vodanhgd@gmail.com>
2026-05-06 23:04:07 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,8 +1,9 @@
 <?php
-
 // config/database.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-// File: config/database.php
-define('DB_NAME', 'pharmacy_db'); 
+
+// Nếu chạy trên Render, lấy thông tin từ Environment Variables
+// Nếu không có, sẽ mặc định dùng thông số localhost (để bạn vẫn code được ở máy cá nhân)
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'pharmacy_db');
